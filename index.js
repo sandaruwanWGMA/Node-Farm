7fe7dc5acb4e77abf9fb5bf3872e96249634400b
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-08-29 16:46:48 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const js = require("http");
 const http = require("http");
+const url = require("url")
 
 // const data = fs.readFileSync("./txt/append.txt", 'utf-8');
 // console.log(data);
@@ -10,29 +11,38 @@ const dataObj = JSON.parse(data);
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8")
 
 const replaceTemplate = (temp, product) => {
-    temp = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    temp = temp.replace(/{%IMAGE%}/g, product.image);
-    temp = temp.replace(/{%PRICE%}/g, product.price);
-    temp = temp.replace(/{%FROM%}/g, product.from);
-    temp = temp.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    temp = temp.replace(/{%QUANTITY%}/g, product.quantity);
-    temp = temp.replace(/{%DESCRIPTION%}/g, product.description);
-    temp = temp.replace(/{%ID%}/g, product.id);
+  temp = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  temp = temp.replace(/{%IMAGE%}/g, product.image);
+  temp = temp.replace(/{%PRICE%}/g, product.price);
+  temp = temp.replace(/{%FROM%}/g, product.from);
+  temp = temp.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  temp = temp.replace(/{%QUANTITY%}/g, product.quantity);
+  temp = temp.replace(/{%DESCRIPTION%}/g, product.description);
+  temp = temp.replace(/{%ID%}/g, product.id);
 
-    if (!product.organic) temp = temp.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return temp;
+  if (!product.organic) temp = temp.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return temp;
 }
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/" || req.url === "/overview") {
-        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, dataObj.map(el => replaceTemplate(tempCard, el)))
-        res.end(output)
-    }
+
+  const {query, pathname} = url.parse(req.url, true);
+
+  if (req.url === "/" || req.url === "/overview") {
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, dataObj.map(el => replaceTemplate(tempCard, el)))
+    res.end(output)
+  } else if (pathname === "/product") {
+    const product = dataObj[query.id];
+    console.log(product)
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
 })
 
 server.listen(8000, "127.0.0.1", () => {
-    console.log("Listening to the server!");
+  console.log("Listening to the server!");
 
 })
